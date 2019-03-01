3fc22a9751a8c38e35a75a262c44e6e5c0d3850e
SerhiyAntipov <serhiy.antipov@gmail.com>
2019-03-01 20:50:57 +0200

    037 commit

--- js/slider_script.js
+++ js/slider_script.js
@@ -1,70 +1,113 @@
-let sliderPage = document.getElementsByClassName('sliderPage');
-let slider = document.querySelector('.slider');
-let centerSlider = Math.floor(sliderPage.length/2);
-let numClick = centerSlider;
-let arrSlider = Array.from(sliderPage);//створюємо масив з псевдомасива 
+window.onload = function(){
+	var icon = document.getElementsByClassName('icon');
+	var startpage = document.getElementsByClassName('startpage');
+	var startNum = 3;
+		startpage[startNum].setAttribute('class', 'startpage active');
+	var StpageActive = startpage[startNum];
+	var arrow = document.getElementsByClassName('arrow');
+	var delta = [];
+		delta.length = startpage.length
 
-
-let slider_wrapp = document.querySelector('#slider_wrapp');
-
-window.onresize = resizeSlider;
-
-resizeSlider();
-function resizeSlider(){
-    let slider_wrapp = document.querySelector('#slider_wrapp');
-    slider_width = slider_wrapp.offsetWidth;
-    slider_wrapp.style.height = slider_wrapp.offsetWidth/3 + "px";
-}
-
-
-for(let i = 0; i< arrSlider.length; i++){
-		arrSlider[i].setAttribute('position', i);
-	}
-
-slider.onclick = function fSlider(event){	
-	if (event.target.className == "sliderPage"){  //перевіряємо чи клік по картинці
-		event.target.setAttribute('slide','click'); //присвоюємо атрибут для відслідковування 
-		for(let i = 0; i< sliderPage.length; i++){ //шукаємо в масиві порядковий номер з атрибутом 'position','1'
-			if(sliderPage[i].getAttribute('slide') ==  'click'){
-				numClick = i;
-				sliderPage[i].removeAttribute('slide');
-			}
-		}
-	}
-	render()
-}
-
-function render(){
-	if(numClick < centerSlider ){
-			for(let i = 0; i< (centerSlider - numClick); i++ ){
-				arrSlider.unshift(arrSlider.pop());
-			}
-		}
-		else if(numClick > centerSlider){
-			for(let i = 0; i< (numClick - centerSlider); i++ ){
-				arrSlider.push(arrSlider.shift());
-			}
-		}
-		centerSlider = numClick;
 		
-	for(let i = 0; i< arrSlider.length; i++){
-		arrSlider[i].setAttribute('position', i);
-	}
-	unHideLinks()
-}
+	for(var i = 0; i<icon.length; i++){
+		icon[i].setAttribute('data-num', i);
+	};
 
-autoSlide ()
+	document.querySelector('.block_arrow').onclick = function clickArrow(event){
+		if(event.target.className == 'arrow left'){
+			Left()
+		};
+		if(event.target.className == 'arrow right'){
+			Right()
+		};
+		StpageChildrenActive();
 
-function autoSlide (){
-	render();
-	numClick == numClick++;
-	if(numClick > arrSlider.length -1) numClick = 0;
- 	setTimeout(autoSlide, 3000);
-}
+	};
 
-function unHideLinks(){
-	for(let i = 0; i< arrSlider.length; i++ ){
-	sliderPage[i].children[0].style.display = 'none';
-	}
-	sliderPage[numClick].children[0].style.display = 'inline-block';
-}
+	document.onkeydown = function keyArrow(event){
+		if(event.code == 'ArrowRight'){
+			Right();
+		}
+		else if(event.code == 'ArrowLeft'){
+			Left();
+		};
+		StpageChildrenActive();
+
+	};	
+	
+	function Right(){
+		if(startNum < startpage.length -1){
+		startpage[startNum].setAttribute('class', 'startpage');
+		startNum = startNum + 1;
+		startpage[startNum].setAttribute('class', 'startpage active');
+		}
+		else{
+		startNum = 0;
+		startpage[startNum].setAttribute('class', 'startpage active');
+		startpage[startpage.length - 1].setAttribute('class', 'startpage');
+		};
+		return false;
+	};
+    
+	function Left(){
+		if(startNum > 0){
+		startpage[startNum].setAttribute('class', 'startpage');
+		startNum = startNum-1;
+		startpage[startNum].setAttribute('class', 'startpage active');
+		}
+		else{
+		startNum = startpage.length - 1;
+		startpage[0].setAttribute('class', 'startpage');
+		startpage[startNum].setAttribute('class', 'startpage active');
+		};
+	};
+
+	StpageChildrenActive();
+	function StpageChildrenActive(){
+		for(var i=0; i<startpage.length; i++){
+			if(startpage[i].getAttribute('class') ==  'startpage active'){
+				icon[i].setAttribute('class', 'icon active')
+				StpageActive = startpage[i];
+				// console.log(StpageActive);
+				var StpageActivePlus = StpageActive.getElementsByClassName('content')
+				// console.log(StpageActivePlus)
+				for(var x =0; x<StpageActivePlus.length; x++){
+					StpageActivePlus[x].setAttribute('class', 'content active' );
+				};
+			}
+			else{
+				icon[i].setAttribute('class', 'icon')
+				var StpageNoActive = startpage[i];
+				// console.log(StpageNoActive);
+				var StpageNoActivePlus = StpageNoActive.getElementsByClassName('content')
+				for(var y =0; y<StpageNoActivePlus.length; y++){
+					StpageNoActivePlus[y].setAttribute('class', 'content' );
+				};
+			};
+		};
+	};
+
+	document.querySelector('.slider_list_ul').onclick = function(event){
+       	if(event.target.className == 'icon'){
+			startNum = +event.toElement.dataset.num;// + для переводу змінної в число
+			for(var x = 0; x<icon.length; x++){
+				if(x == startNum ){
+				startpage[x].setAttribute('class', 'startpage active');
+				}
+				else{
+				startpage[x].setAttribute('class', 'startpage');
+				};
+			};
+		};
+		StpageChildrenActive();
+	};
+    
+    
+    function autoSlide(){
+        Right()
+        StpageChildrenActive()
+    }
+    setInterval(autoSlide, 3000)
+   
+};
+
