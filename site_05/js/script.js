7ac179894bf1b83cae2baac6be7ced72651bbb9b
SerhiyAntipov <serhiy.antipov@gmail.com>
2019-06-03 19:40:19 +0300

    site_05_no_adaptive

--- site_05/js/script.js
+++ site_05/js/script.js
@@ -16,4 +16,45 @@ window.onload = function() {
     });
 
 
+//Slider------------    
+    Slider();
+
+    function Slider() {
+        var sliderPhoto = document.querySelectorAll('.slider-photo');
+        var sliderBack = document.querySelector('.back');
+        var sliderForward = document.querySelector('.forward');
+
+        sliderBack.addEventListener('click', function() {
+            for (var i = 0; i < sliderPhoto.length; i++) {
+                if (sliderPhoto[i].classList.value == 'slider-photo slider-active') {
+                    if (i + 1 < sliderPhoto.length) {
+                        sliderPhoto[i].classList.toggle('slider-active');
+                        sliderPhoto[i + 1].classList.toggle('slider-active');
+                    } else {
+                        sliderPhoto[sliderPhoto.length - 1].classList.toggle('slider-active');
+                        sliderPhoto[0].classList.toggle('slider-active');
+                    }
+                    return;
+                }
+            }
+        });
+
+        sliderForward.addEventListener('click', function() {
+            for (var i = 0; i < sliderPhoto.length; i++) {
+                if (sliderPhoto[i].classList.value == 'slider-photo slider-active') {
+                    if (i - 1 >= 0) {
+                        sliderPhoto[i].classList.toggle('slider-active');
+                        sliderPhoto[i - 1].classList.toggle('slider-active');
+                    } else {
+                        sliderPhoto[0].classList.toggle('slider-active');
+                        sliderPhoto[sliderPhoto.length - 1].classList.toggle('slider-active');
+                    }
+                    return;
+                }
+            }
+        });
+    }
+//------------------
+    
+    
 };
