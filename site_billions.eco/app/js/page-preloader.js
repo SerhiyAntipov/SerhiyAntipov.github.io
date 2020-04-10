adcc069beb9c9162619b80ee88096f1e5b61f624
Serhiy Antipov <serhiy.antipov@gmail.com>
2020-04-10 16:13:52 +0300

    restructuring delete sites

--- site_billions.eco/app/js/page-preloader.js
+++
@@ -1,8 +0,0 @@
-//
-//++++++++++++++ PAGE PRELOADER ++++++++++++++++++++++++++++++++++++++++++++++
-$(window).on('load', function () {
-    let $preloader = $('.page-preloader'),
-        $spinner   = $preloader.find('.spinner');
-    $spinner.delay(1500).fadeOut('slow');
-    $preloader.delay(1500).fadeOut('slow');
-});
--- site_cafe/app/js/smooth-scroll.js
+++
@@ -1,9 +0,0 @@
- //
- //++++++++++++++ SMOOTH SCROLL +++++++++++++++++++++++++++++++++++++++++++++++++++
- (function () {
-     let scroll = new SmoothScroll('a[href*="#"]', {
-         ignore: '[data-scroll-ignore]',
-         speed: 500,
-         offset: 45
-     });
- })();
